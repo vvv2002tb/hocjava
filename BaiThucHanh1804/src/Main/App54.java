a892e369ce29894dc324519093e1bef3b89e7d5f
vvv2002tb <92530486+vvv2002tb@users.noreply.github.com>
2022-04-18 17:59:26 +0700

    BaiThucHanh1804

---
+++ BaiThucHanh1804/src/Main/App54.java
@@ -0,0 +1,44 @@
+package Main;
+import shape.HinhChuNhat;
+
+import shape.HinhTron;
+import shape.HinhTru;
+import shape.HinhVuong;
+public class App54 {
+    public static void main(String[] args) {
+        //Thử nghiệm với lớp Hình Tròn
+        HinhTron hinhTron = new HinhTron();
+        hinhTron.xuatTen();
+        hinhTron.nhapbanKinh();
+        hinhTron.tinhChuVi();
+        hinhTron.tinhDienTich();
+        hinhTron.inChuVi();
+        hinhTron.inDienTich();
+
+        //Thử nghiệm với lớp Hình Trụ
+        HinhTru hinhTru = new HinhTru();
+        hinhTru.xuatTen();
+        hinhTru.nhapChieuCao();
+        hinhTru.tinhTheTich();
+        hinhTru.inTheTich();
+
+        //Thử nghiệm với lớp Hình Chữ Nhật
+        HinhChuNhat hinhChuNhat = new HinhChuNhat();
+        hinhChuNhat.xuatTen();
+        hinhChuNhat.nhapChieuDai();
+        hinhChuNhat.nhapChieuRong();
+        hinhChuNhat.tinhChuVi();
+        hinhChuNhat.tinhDienTich();
+        hinhChuNhat.inChuVi();
+        hinhChuNhat.inDienTich();
+
+        //Thử nghiệm với lớp Hình Vuông
+        HinhVuong hinhVuong = new HinhVuong();
+        hinhVuong.xuatTen();
+        hinhVuong.nhapCanh();
+        hinhVuong.tinhChuVi();
+        hinhVuong.tinhDienTich();
+        hinhVuong.inChuVi();
+        hinhVuong.inDienTich();
+    }
+}
